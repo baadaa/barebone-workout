d2128bc9e692f53cc910354609b4b583cb461a70
Bumhan Yu <bumhan.yu@gmail.com>
2022-03-24 02:47:05 -0400

    add two more workouts

--- components/Workouts/index.tsx
+++ components/Workouts/index.tsx
@@ -2,6 +2,8 @@ export * from './Rest';
 export * from './TensAndTwos';
 export * from './AgilityCircles';
 export * from './AlternatingSupermans';
+export * from './CurtsyLunges';
+export * from './DiagonalLunges';
 export * from './SidePlank';
 export * from './BalanceReaches';
 export * from './Bicycles';
